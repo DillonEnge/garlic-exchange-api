5bd2b5e4d8a11d5363d3e8db08e1f69330a79fdb
Dillon Enge <dillon.enge@gmail.com>
2018-02-22 17:19:05 -0500

    N

--- server.py
+++ server.py
@@ -12,8 +12,8 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40))
-    email = db.Column(db.String(80), unique=True)
+    name = db.Column(db.String(60))
+    email = db.Column(db.String(120), unique=True)
 
     def __init__(self, name, email):
         self.name = name
