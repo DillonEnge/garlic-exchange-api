258af25e6ae3f0690cc8c72d8ecf240ffb3eb7dc
Dillon Enge <dillon.enge@gmail.com>
2018-03-01 04:08:34 -0500

    chchch

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-db.create_all()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
