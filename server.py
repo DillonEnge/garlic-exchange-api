f5a57ce971aff95ded404915d7c1fc7787af7bfe
Dillon Enge <dillon.enge@gmail.com>
2018-03-02 16:20:14 -0500

    Updated Stuff.

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 # server.py
 import os, random, requests
-
+import base64, hashlib
 from pybitcoin import AddressFetcher
-from flask import Flask, render_template, request
+from functools import wraps
+from flask import Flask, render_template, request, abort
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
@@ -39,6 +40,16 @@ class Wallets(db.Model):
     def __repr__(self):
         return '<Tx %s, Public %r>' % (self.tx, self.public)
 
+class Keys(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(120), unique=True)
+
+    def __init__(self, key):
+        self.key = key
+
+    def __repr__(self):
+        return '<Key %s>' % (self.id)
+
 def addToDB(obj):
     db.session.add(obj)
     db.session.commit()
@@ -51,18 +62,33 @@ def get_hello():
   greeting_list = ['Ciao', 'Hei', 'Salut', 'Hola', 'Hallo', 'Hej']
   return random.choice(greeting_list)
 
-@app.route("/price")
+def generate_hash_key():
+    return base64.b64encode(hashlib.sha256(str(random.getrandbits(256))).digest(),
+                            random.choice(['rA', 'aZ', 'gQ', 'hH', 'hG', 'aR', 'DD'])).rstrip('==')
+
+# The actual decorator function
+def require_appkey(view_function):
+    @wraps(view_function)
+    # the new, post-decoration function. Note *args and **kwargs here.
+    def decorated_function(*args, **kwargs):
+        if request.form['key'] and request.form['key'] == Keys.query.filter_by(id=1).first().key:
+            return view_function(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated_function
+
+@app.route("/price", methods=['GET'])
 def price():
     coin = request.args.get('coin', 'USD')
     response = requests.get('https://min-api.cryptocompare.com/data/price?fsym=GRLC&tsyms=' + coin)
     return str(response.json()[coin])
 
-@app.route("/transaction/<int:tx>")
-def address(tx):
+@app.route("/create/address", methods=['POST'])
+@require_appkey
+def createAddress():
     try:
+        tx = request.form['tx']
         gAddress = AddressFetcher().generateGarlicAddress(True)
-        print(gAddress[0])
-        print(gAddress[1])
         wallet = Wallets(tx=tx, public=gAddress[0], private=gAddress[1])
         addToDB(wallet)
     except:
@@ -70,15 +96,30 @@ def address(tx):
 
     return 'Success'
 
-@app.route("/get/transaction/<int:tx>")
+@app.route("/delete/address", methods=['DELETE'])
+@require_appkey
+def deleteAddress():
+    try:
+        tx = request.form['tx']
+        wallet = Wallets.query.filter_by(tx=tx).first()
+        deleteFromDB(wallet)
+    except:
+        return "Unexpected error"
+
+    return 'Success'
+
+@app.route("/get/address/<int:tx>", methods=['GET'])
 def getAddress(tx):
     try:
-        transaction = Wallets.query.filter_by(tx=tx).first()
-        return str(transaction)
+        address = Wallets.query.filter_by(tx=tx).first()
+        if not address:
+            return 'Address from tx:%r not found' % (tx)
+        return str(address.public)
     except:
         return 'Unexpected error'
 
 @app.route("/create/user", methods = ['POST'])
+@require_appkey
 def createUser():
     try:
         name = request.form['name']
